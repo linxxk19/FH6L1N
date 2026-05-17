704dcafc7d972644e3ceb4c219dd6c2de2ef792c
linxxk19 <linxxk19@gmail.com>
2026-05-17 22:43:15 +0800

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -21,7 +21,7 @@ $StartLine = [Console]::CursorTop
 Write-Host "[*][SECURITY CHK]" -ForegroundColor Yellow -NoNewline
 $InputPassword = Read-Host " -> Please Enter Access Password"
 
-if ($InputPassword -ne "0310czc") {
+if ($InputPassword -ne "1315zc") {
     Write-Host "X Password Wrong! Exit." -ForegroundColor Red
     Start-Sleep -Seconds 3
     Exit
